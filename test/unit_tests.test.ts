5206303b6760753669c1337e6e0b4020225dce0c
Paul den Boer <paul@levarne.nl>
2023-04-24 21:01:00 +0200

    added tests and github actions

---
+++ test/unit_tests.test.ts
@@ -0,0 +1,52 @@
+import { ClassAssignmentAvans } from "../ClassAssignmentAvans";
+
+describe("ShippingCosts", () => {
+  let classAssignmentAvans: ClassAssignmentAvans;
+
+  beforeEach(() => {
+    classAssignmentAvans = new ClassAssignmentAvans();
+  });
+
+  it("should return 100 for Ground shipping when calculateShippingCosts is true and totalPrice is less than or equal to 1500", () => {
+    const result = classAssignmentAvans.ShippingCosts(true, "Ground", 1000);
+    expect(result).toEqual(100);
+  });
+
+  it("should return 50 for InStore shipping when calculateShippingCosts is true and totalPrice is less than or equal to 1500", () => {
+    const result = classAssignmentAvans.ShippingCosts(true, "InStore", 1000);
+    expect(result).toEqual(50);
+  });
+
+  it("should return 250 for NextDayAir shipping when calculateShippingCosts is true and totalPrice is less than or equal to 1500", () => {
+    const result = classAssignmentAvans.ShippingCosts(true, "NextDayAir", 1000);
+    expect(result).toEqual(250);
+  });
+
+  it("should return 125 for SecondDayAir shipping when calculateShippingCosts is true and totalPrice is less than or equal to 1500", () => {
+    const result = classAssignmentAvans.ShippingCosts(
+      true,
+      "SecondDayAir",
+      1000
+    );
+    expect(result).toEqual(125);
+  });
+
+  it("should return 0 when calculateShippingCosts is true and totalPrice is greater than 1500", () => {
+    const result = classAssignmentAvans.ShippingCosts(true, "Ground", 2000);
+    expect(result).toEqual(0);
+  });
+
+  it("should return 0 when calculateShippingCosts is false", () => {
+    const result = classAssignmentAvans.ShippingCosts(false, "Ground", 1000);
+    expect(result).toEqual(0);
+  });
+
+  it("should return 0 when typeOfShippingCosts is not recognized", () => {
+    const result = classAssignmentAvans.ShippingCosts(
+      true,
+      "InvalidShippingType",
+      1000
+    );
+    expect(result).toEqual(0);
+  });
+});
